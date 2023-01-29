421b5f0e034dfb44e7cde5d0139cc17e983db51b
AriaHemmati <prefix.suffix.aria@gmail.com>
2023-01-30 01:17:42 +0330

    Starting XD

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define Kill printf("Invalid Response")
+
+int main()
+{
+    Kill;
+    return 0;
+}
